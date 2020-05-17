544dda0a1a5bfa4bd398686c74c7400069d61c66
Joseph Cooper <josephcooper3@gmail.com>
2020-05-17 19:37:37 +0100

    [initial commit] server bootstrap

---
+++ server/freezerservice/src/main/java/com/example/freezerservice/FreezerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.freezerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreezerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreezerserviceApplication.class, args);
+	}
+
+}
---
+++ server/freezerservice/src/test/java/com/example/freezerservice/FreezerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freezerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreezerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
