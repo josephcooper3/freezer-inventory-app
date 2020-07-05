fe4a22c84aee185f009a84abefb227a5bb1c1ccc
Joseph Cooper <josephcooper3@gmail.com>
2020-07-05 20:58:55 +0100

    [Update] adds grid to MainView

--- server/freezerservice/src/main/java/com/example/freezerservice/ui/MainView.java
+++ server/freezerservice/src/main/java/com/example/freezerservice/ui/MainView.java
@@ -1,13 +1,27 @@
 package com.example.freezerservice.ui;
 
+import com.example.freezerservice.backend.models.FoodItem;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private Grid<FoodItem> grid = new Grid<>(FoodItem.class);
 
+    public MainView() {
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
+    }
+
+    private void configureGrid() {
+        grid.addClassName("fooditem-grid");
+        grid.setSizeFull();
+        grid.setColumns("itemName", "foodType", "quantity", "unit", "dateFrozen", "notes");
     }
 
 }
