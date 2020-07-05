7e999d6fbf569592a222874b4f4d6b7ecb7888e1
Joseph Cooper <josephcooper3@gmail.com>
2020-07-05 19:52:49 +0100

    [Update] adds MainView class

---
+++ server/freezerservice/src/main/java/com/example/freezerservice/ui/MainView.java
@@ -0,0 +1,13 @@
+package com.example.freezerservice.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+
+}
